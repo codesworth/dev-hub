9eb741cea024593ea45233e2c737ad9d6e598377
Codesworth <codezworth@gmail.com>
2019-03-23 02:48:41 +0000

    Added Routes and validation

--- validation/login.js
+++ validation/login.js
@@ -1,12 +1,33 @@
 const validator = require('validator');
 const isEmpty = require('../validation/is_empty')
 
-module.exports = function validateLogInInput(data) {
+module.exports = function validateProfileInput(data) {
     let errors = {}
 
 
-    data.email = !isEmpty(data.email) ? data.email : '';
-    data.password = !isEmpty(data.password) ? data.password : '';
+    data.handle = !isEmpty(data.handle) ? data.handle : '';
+    data.status = !isEmpty(data.status) ? data.status : '';
+    data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+
+    if (!validator.isLength(data.handle, {
+            max: 40,
+            min: 2
+        })) {
+        errors.handle = "Handle should be between 2 and 40 charatcers"
+    }
+
+    if (validator.isEmpty(data.handle)) {
+        errors.handle = "Handle is required"
+    }
+
+    if (validator.isEmpty(data.status)) {
+        errors.status = "Status is required"
+    }
+
+    if (validator.isEmpty(data.skills)) {
+        errors.status = "Status is required"
+    }
 
 
 
